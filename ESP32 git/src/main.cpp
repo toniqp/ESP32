52d93e48da8fdaea4b913c062dd729cb924096d3
DESKTOP-AKI32M6\Robocode 8 <robocodekh@gmail.com>
2025-04-12 16:35:46 +0300

    Callback

--- ESP32 git/src/main.cpp
+++ ESP32 git/src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
+#include <esp_bt_device.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BluetoothSerial SerialBT;
+
+void printDeviceAddress();
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  
+  if (!SerialBT.begin("ESP32")) {
+    Serial.println("An error occurred during Bluetooth initialization!");
+  } else {
+    Serial.println("Bluetooth initialized.");
+  }
+  printDeviceAddress();
+  SerialBT.register_callback(callback);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void printDeviceAddress() {
+  const uint8_t* address = esp_bt_dev_get_address();
+  
+  for (int i = 0; i < 6; i++) {
+    char str[3];
+    sprintf(str, "%02X", (int)address[i]);
+    Serial.print(str);
+    
+    if (i < 5) {
+      Serial.print(":");
+    } else {
+      Serial.println();
+    }
+  }
+}
+
+void callback(esp_spp_cb_event_t event, esp_spp_cb_event_t *param){
+  if(event == ESP_SPP_SRV_OPEN_EVT){
+    Serial.println("Client Connected");
+  }
 }
